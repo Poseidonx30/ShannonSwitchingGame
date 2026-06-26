75912ba8ad717582525f28032f9faad8882fa25c
Levin <lallalouf@gmail.com>
2026-06-26 18:26:35 +0200

    Finale Änderungen

--- src/Wettbewerbsstrategie.jl
+++ src/Wettbewerbsstrategie.jl
@@ -1,9 +1,38 @@
-const TEAM_NAME::String = ""
+const TEAM_NAME::String = "StockFisch 1.0"
+
+mutable struct ExtendedGameState
+    graph::EfficientGameGraph
+    short_Graph::EfficientGameGraph
+    A::Base.Set{Edge} 
+    B::Base.Set{Edge} 
+    e1::Edge
+    has_winning_strategy::Symbol
+    current_player::Symbol
+    history::Vector{Tuple{Symbol, Edge}} 
+    imaginary_moves::Base.Set{Edge}
+    winner::Union{Symbol, Nothing}
+end
+
+mutable struct EfficientGameGraph
+    edges::Base.Set{Edge}  
+    s::Int                 
+    t::Int                 
+end
+
+const EXTENDED_STATE = Ref{Union{Nothing, ExtendedGameState}}(nothing)
+
 
 function weighted_short(state::GameState)::Edge
-    return
+    if isempty(state.history)
+    I_can_win = who_can_win() == :short ?...
 end
 
 function weighted_cut(state::GameState)::Edge 
-    return
+    
 end
+
+function MCTS(state::GameState; Zeitlimit = 1.0)
+     
+end 
+
+
